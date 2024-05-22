74f84a817fba2a30faf68f4fe135c07005b7f05b
bmac <bmac325@gmail.com>
2024-05-21 20:33:08 -0400

    Add pending e2e test:

---
+++ features/support/steps.ts
@@ -0,0 +1,12 @@
+import { When, Then } from '@cucumber/cucumber';
+
+When('I visit octocat\'s page', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('I should see .gitignore listed first', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
