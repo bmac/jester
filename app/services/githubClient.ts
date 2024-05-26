77b302072cef8d36f3ea32f8251d417f533ffb54
bmac <bmac325@gmail.com>
2024-05-26 15:03:51 -0400

    Implement githubClient

--- app/services/githubClient.ts
+++ app/services/githubClient.ts
@@ -1,5 +1,47 @@
+import { graphql } from "@octokit/graphql";
 
+export type Gist = {
+    description: string;
+    stargazerCount: number;
+};
 
-export type Gist = {stargazerCount: number};
+export const getGistsForUser = async (username: string) => {
+    const response = await graphql<{
+        user: {
+            gists: {
+                nodes: Gist[]
+            }
+        }
 
-export const getGistsForUser = async (username: string) => [] as (Gist[]);
+    }>(`
+{
+  user(login: "${username}") {
+    gists(first: 100) {
+      nodes {
+        id
+        name
+        description
+        stargazerCount
+        url
+        createdAt
+        updatedAt
+        files {
+          name
+          language {
+            name
+          }
+        }
+      }
+    }
+  }
+}
+`,
+        {
+            headers: {
+                authorization: `token ${process.env.GITHUB_TOKEN}`,
+            },
+        },
+    );
+
+    return response.user.gists.nodes;
+};
---
+++ test/vcr.ts
@@ -0,0 +1,7 @@
+import { join } from 'node:path';
+import { VCR, FileStorage } from 'vcr-test';
+
+export const vcr = new VCR(new FileStorage(join(__dirname, '__cassettes__')));
+vcr.requestMasker = (req) => {
+  req.headers['authorization'] = 'masked';
+};
