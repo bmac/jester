1f929f5f6a5c77d696061364daed02d19ebb59d9
bmac <bmac325@gmail.com>
2024-05-26 20:22:35 -0400

    Fix e2e tests

--- features/support/steps.ts
+++ features/support/steps.ts
@@ -1,12 +1,12 @@
 import assert from "assert";
 import { When, Then } from "@cucumber/cucumber";
-import { topGistForUser } from "../../app/services/gistService.ts";
+import { topGistForUser } from "../../app/services/gistService";
 
-When("I visit octocat's page", function () {
-  this.topGists = topGistForUser("octocat");
+When("I visit octocat's page", async function () {
+  this.topGists = await topGistForUser("octocat");
 });
 
 Then("I should see .gitignore listed first", function () {
   const topGist = this.topGists[0];
-  assert.equal(topGist.name, ".gitignore");
+  assert.equal(topGist.description, "Some common .gitignore configurations");
 });
