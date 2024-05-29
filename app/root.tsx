7d35d8f9c4c04fd7e8374dc95b2df77f9f2d2c20
bmac <bmac325@gmail.com>
2024-05-28 22:03:31 -0400

    more stylish

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <header>
-          🎭 Jester{" "}
+          <span>🎭 Jester{" "}</span>
           <span className={styles.subHeading}>
             {params.username
               ? `gist stars for ${params.username}`
