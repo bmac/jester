28bbde8c2138a3942b51bdc4d28ce225aa6b8b23
bmac <bmac325@gmail.com>
2024-06-01 17:32:33 -0400

    Add playwright test runner tests

---
+++ e2e/stared_gists.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('User\'s gist are sorted by stars', async ({ page }) => {
+    await page.goto("http://localhost:5173/octocat");
+    const titleNode = page.getByTestId("card-filename-0");
+    expect(await titleNode.innerText()).toBe(".gitignore");
+});
+
