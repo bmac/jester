50c06b698287f5999fb59e78d006da78da20203c
bmac <bmac325@gmail.com>
2024-06-16 10:52:55 -0400

    Add documentation

--- test/stub.ts
+++ test/stub.ts
@@ -6,6 +6,24 @@ type RecursivePartial<T> = {
     T[P];
 };
 
+/**
+   Identity function that casts an object to the specified type. Used
+   for creating a stub value in a unit test that doesn't have some
+   required property that isn't needed by the behavior under test so
+   its addition adds noise to the test setup.
+
+   In may cases you can use `as Type` to get the same behavior with
+   less code but TypeScript's rules don't always let you do that with
+   more complex types. `stub` is there for those cases so you don't
+   have to case through unknown and loose type information.
+
+   Example:
+   type User = { id: number; name: string; };
+
+   // name isn't a concern of this test so we use stub to omit it
+   const negativIdUser = stub<User>({id: -1});
+   expect(isValidUser(negativIdUser)).toBe(false);
+ */
 export const stub = <T extends object>(descriptor?: RecursivePartial<T>) => {
   return { ...descriptor } as T;
 };
